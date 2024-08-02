f40aa5f722a3b354cf4f8f3433a303f8d28a63cc
babakfp <babak.bxf@gmail.com>
2024-08-02 22:06:05 +0330

    Added Recursive font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,17 @@
 <script lang="ts">
     import "$lib/app.css"
+    import "@fontsource-variable/recursive/mono.css"
+    import recursiveMonoUrl from "@fontsource-variable/recursive/files/recursive-latin-mono-normal.woff2?url"
 </script>
+
+<svelte:head>
+    <link
+        rel="preload"
+        as="font"
+        type="font/woff2"
+        href={recursiveMonoUrl}
+        crossorigin="anonymous"
+    />
+</svelte:head>
 
 <slot />
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,19 @@
+import { omit } from "remeda"
 import type { Config } from "tailwindcss"
 import { allAddons } from "tailwindcss-addons"
+import { fontWeight } from "tailwindcss/defaultTheme"
 
 export default {
     content: ["./src/**/*.{html,svelte}"],
+    theme: {
+        fontFamily: {
+            sans: ["Recursive Variable"],
+            mono: ["Recursive Variable", { fontVariationSettings: "'MONO' 1" }],
+        },
+        fontWeight: {
+            ...omit(fontWeight, ["thin", "extralight"]),
+            extrablack: "1000",
+        },
+    },
     plugins: [...allAddons()],
 } satisfies Config
