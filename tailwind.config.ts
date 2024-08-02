b019d7a6957c70ccad4dab154a3bb9b1ae317a94
babakfp <babak.bxf@gmail.com>
2024-08-02 22:07:30 +0330

    Changed gray color to zinc. Added favicon colors. Added container config.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,26 @@
 import { omit } from "remeda"
 import type { Config } from "tailwindcss"
 import { allAddons } from "tailwindcss-addons"
+import { zinc } from "tailwindcss/colors"
 import { fontWeight } from "tailwindcss/defaultTheme"
 
 export default {
     content: ["./src/**/*.{html,svelte}"],
     theme: {
+        extend: {
+            colors: {
+                gray: zinc,
+                favicon: {
+                    100: "#f2c029",
+                    200: "#f2921d",
+                    300: "#f26938",
+                },
+            },
+            container: {
+                center: true,
+                padding: "1rem",
+            },
+        },
         fontFamily: {
             sans: ["Recursive Variable"],
             mono: ["Recursive Variable", { fontVariationSettings: "'MONO' 1" }],
