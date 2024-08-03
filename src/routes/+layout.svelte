32537beb6e319ab7f326333299e3296418d4689a
babakfp <babak.bxf@gmail.com>
2024-08-04 02:34:34 +0330

    Added footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -26,3 +26,30 @@
 <main class="container pb-16 pt-8">
     <slot />
 </main>
+
+<footer class="border-t border-gray-800 py-8">
+    <ul class="container list-inside list-disc space-y-2 text-sm">
+        <li>
+            Made by
+            <a class="underline" href="https://babakfp.ir" target="_blank">
+                Babak
+            </a>
+        </li>
+        <li>
+            Source code on
+            <a
+                class="underline"
+                href="https://github.com/babakfp/MyApiList"
+                target="_blank"
+            >
+                GitHub
+            </a>
+        </li>
+        <li>
+            Favicon by
+            <a class="underline" href="https://flaticon.com" target="_blank">
+                FlatIcon
+            </a>
+        </li>
+    </ul>
+</footer>
