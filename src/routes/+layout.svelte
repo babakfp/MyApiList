50078e7b1a9c0527a0dc84a21ae50f1b87fa6968
babakfp <babak.bxf@gmail.com>
2025-04-10 02:44:28 +0330

    .

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,30 +1,9 @@
 <script lang="ts">
     import "$lib/app.css"
-    import recursiveMonoUrl from "@fontsource-variable/recursive/files/recursive-latin-mono-normal.woff2?url"
     import Footer from "$lib/components/Footer.svelte"
 
     let { children } = $props()
 </script>
-
-<svelte:head>
-    <link
-        rel="preload"
-        as="font"
-        type="font/woff2"
-        href={recursiveMonoUrl}
-        crossorigin="anonymous"
-    />
-    {@html `<style>
-        @font-face {
-            font-family: 'Recursive Variable';
-            font-style: normal;
-            font-display: block;
-            font-weight: 300 1000;
-            src: url(${recursiveMonoUrl}) format('woff2-variations');
-            unicode-range: U+0000-00FF,U+0131,U+0152-0153,U+02BB-02BC,U+02C6,U+02DA,U+02DC,U+0304,U+0308,U+0329,U+2000-206F,U+2074,U+20AC,U+2122,U+2191,U+2193,U+2212,U+2215,U+FEFF,U+FFFD;
-        }
-    </style>`}
-</svelte:head>
 
 <main class="container pt-8 pb-16">
     {@render children()}
