14185f36074601ba88bed78cb693c577a6534e32
babakfp <babak.bxf@gmail.com>
2025-04-10 02:42:33 +0330

    .

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,34 @@
+<footer class="border-t border-gray-800 py-8">
+    <ul class="container list-inside list-disc space-y-2 text-sm">
+        <li>
+            Made by
+            <a
+                class="underline focus-visible:text-yellow-500"
+                href="https://babakfp.ir"
+                target="_blank"
+            >
+                Babak
+            </a>
+        </li>
+        <li>
+            Source code on
+            <a
+                class="underline focus-visible:text-yellow-500"
+                href="https://github.com/babakfp/MyApiList"
+                target="_blank"
+            >
+                GitHub
+            </a>
+        </li>
+        <li>
+            Favicon by
+            <a
+                class="underline focus-visible:text-yellow-500"
+                href="https://flaticon.com"
+                target="_blank"
+            >
+                FlatIcon
+            </a>
+        </li>
+    </ul>
+</footer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import "$lib/app.css"
     import recursiveMonoUrl from "@fontsource-variable/recursive/files/recursive-latin-mono-normal.woff2?url"
+    import Footer from "$lib/components/Footer.svelte"
 
     let { children } = $props()
 </script>
@@ -29,37 +30,4 @@
     {@render children()}
 </main>
 
-<footer class="border-t border-gray-800 py-8">
-    <ul class="container list-inside list-disc space-y-2 text-sm">
-        <li>
-            Made by
-            <a
-                class="underline focus-visible:text-yellow-500"
-                href="https://babakfp.ir"
-                target="_blank"
-            >
-                Babak
-            </a>
-        </li>
-        <li>
-            Source code on
-            <a
-                class="underline focus-visible:text-yellow-500"
-                href="https://github.com/babakfp/MyApiList"
-                target="_blank"
-            >
-                GitHub
-            </a>
-        </li>
-        <li>
-            Favicon by
-            <a
-                class="underline focus-visible:text-yellow-500"
-                href="https://flaticon.com"
-                target="_blank"
-            >
-                FlatIcon
-            </a>
-        </li>
-    </ul>
-</footer>
+<Footer />
