f26963f24ee87325a13c44ffee0268c1bf4cf4ec
babakfp <babak.bxf@gmail.com>
2025-04-10 14:31:43 +0330

    .

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -13,11 +13,11 @@ export const apis: API[] = db
 
 export const apiMetadata = (() => {
     const labels = [
-        ...new Set(apis.flatMap((p) => Object.keys(p.metadata))),
+        ...new Set(apis.flatMap((a) => Object.keys(a.metadata))),
     ] as MetadataLabel[]
     const result = labels.map((label) => ({
         label,
-        values: [...new Set(apis.map((p) => p.metadata[label]))],
+        values: [...new Set(apis.map((a) => a.metadata[label]))],
     }))
     return result
 })()
